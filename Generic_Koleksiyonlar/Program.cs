3ee6dbbc036c361cce42c7f2f34ad4ffbd620372
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-18 18:00:11 +0300

    C# Generic Koleksiyon1 Yapıldı.

--- Generic_Koleksiyonlar/Program.cs
+++ Generic_Koleksiyonlar/Program.cs
@@ -10,6 +10,40 @@ namespace Generic_Koleksiyonlar
     {
         static void Main(string[] args)
         {
+            // <T> : T tipi demek. .net framework içerisinde olan veya bizim oluşturduğumuz herhangibir tip demek.
+
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            List<string > list2 = new List<string>();
+            list2.Add("Halil");
+            list2.Add("Hamza");
+            list2.Add("Hakan");
+
+            for(int i = 0  ; i < list2.Count ; i++)
+            {
+                Console.WriteLine(list2[i]);
+            }
+                 
+            List<musteri> musteris = new List<musteri>();
+            musteris.Add(new musteri()
+            {
+                id = 1,
+                isim="Halil",
+                soyisim = "Özcan"
+            });
+
+            foreach(musteri item in musteris)
+            {
+                Console.WriteLine(item.isim);
+            }
         }
     }
 }
---
+++ Generic_Koleksiyonlar/musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Koleksiyonlar
+{
+    public class musteri
+    {
+        public int id { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+    }
+}
